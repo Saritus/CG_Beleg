7760c271d82aedd1e5df60c98df3d0d3c5ca90d9
IMGSaibh <mathiasgewissen@gmx.de>
2016-05-03 22:39:41 +0200

    1 Commit

--- src/LineareAlgebra.java
+++ src/LineareAlgebra.java
@@ -4,93 +4,169 @@
 abstract public class LineareAlgebra
 {
 
-    Vector add2D(Vector2D vec1, Vector2D vec2){
+    static Vector add2D(Vector2D vec1, Vector2D vec2){
         Vector2D vec3 = new Vector2D(vec1);
         vec3.add(vec2);
         return vec3;
     }
 
-    Vector add3D(Vector3D vec1, Vector3D vec2){
+    static Vector add3D(Vector3D vec1, Vector3D vec2){
         Vector3D vec3 = new Vector3D(vec1);
         vec3.add(vec2);
         return vec3;
     }
 
 
-    Vector sub2D(Vector2D vec1, Vector2D vec2){
+    static Vector sub2D(Vector2D vec1, Vector2D vec2){
         Vector2D vec3 = new Vector2D(vec1);
         vec3.sub(vec2);
         return vec3;
     }
 
-    Vector sub3D(Vector3D vec1, Vector3D vec2){
+    static Vector sub3D(Vector3D vec1, Vector3D vec2){
         Vector3D vec3 = new Vector3D(vec1);
         vec3.sub(vec2);
         return vec3;
     }
 
-    Vector mult2D(Vector2D vec1, double scalar){
+    static Vector mult2D(Vector2D vec1, double scalar){
         Vector2D vec2 = new Vector2D(vec1);
         vec2.mult(scalar);
         return vec2;
     }
 
-    Vector mult3D(Vector3D vec1, double scalar){
+    static Vector mult3D(Vector3D vec1, double scalar){
         Vector3D vec2 = new Vector3D(vec1);
         vec2.mult(scalar);
         return vec2;
     }
 
 
-    Vector div2D(Vector2D vec1,double scalar){
+    static Vector div2D(Vector2D vec1,double scalar){
         Vector2D vec2 = new Vector2D(vec1);
         vec2.div(scalar);
         return vec2;
     }
 
-    Vector div3D(Vector3D vec1,double scalar){
+    static Vector div3D(Vector3D vec1,double scalar){
         Vector3D vec2 = new Vector3D(vec1);
         vec2.div(scalar);
         return vec2;
     }
 
-    boolean isEqual2D(Vector2D vector1, Vector2D vector2){
-        if(vector1.isEqual(vector2))
+    static boolean isEqual2D(Vector2D vec1, Vector2D vec2){
+        if(vec1.isEqual(vec2))
             return true;
         else
             return false;
     }
 
-    boolean isEqual3D(Vector3D vector1, Vector3D vector2){
-        if(vector1.isEqual(vector2))
+    static boolean isEqual3D(Vector3D vec1, Vector3D vec2){
+        if(vec1.isEqual(vec2))
             return true;
         else
             return false;
     }
 
-    boolean isNotEqual(Vector2D vector, Vector2D vector1){
-        for (int i = 0; i < vector.length; i++){
-            if(vector.vectorelements[i] != vector1.vectorelements[i]){
-                return true;
-            }
+    static boolean isNotEqual(Vector3D vec1, Vector3D vec2){
+        if(vec1.isNotEqual(vec2))
+            return true;
+        else
+            return false;
+    }
+
+    static double veclength2D(Vector2D vec) {
+        return vec.veclength();
+    }
+
+    static double veclength3D(Vector3D vec) {
+        return vec.veclength();
+    }
+
+    static Vector normalize2D(Vector2D vec){
+        vec.normalize();
+        return vec;
+    }
+    static Vector normalize3D(Vector2D vec){
+        vec.normalize();
+        return vec;
+    }
+
+    static double euclideandist2D(Vector2D vec1,Vector2D vec2){
+        double distance = 0;
+        Vector vec3 = sub2D(vec2,vec1);
+        for (int i = 0; i < vec3.length;i++){
+            distance += Math.pow(vec3.vectorelements[i],2);
         }
-        return false;
+        return Math.sqrt(distance);
+    }
+    static double euclideandist3D(Vector3D vec1,Vector3D vec2){
+        double distance = 0;
+        Vector vec3 = sub3D(vec2,vec1);
+        for (int i = 0; i < vec3.length;i++){
+            distance += Math.pow(vec3.vectorelements[i],2);
+        }
+        return Math.sqrt(distance);
+    }
+    static double manhattendist2D(Vector2D vec1, Vector2D vec2){
+        double dist = 0;
+        Vector vec = sub2D(vec1,vec2);
+        for (int i = 0; i < vec1.length; i++){
+            dist += vec.vectorelements[i];
+        }
+        return dist;
+    }
+    static double manhattendist3D(Vector3D vec1, Vector3D vec2){
+        double dist = 0;
+        Vector vec = sub3D(vec1,vec2);
+        for (int i = 0; i < vec1.length; i++){
+            dist += vec.vectorelements[i];
+        }
+        return dist;
     }
 
-    double veclength2D(Vector2D vector) {
-        return vector.veclength();
+    static double crossProduct2D(Vector2D vec1, Vector2D vec2){
+        double det = 0;
+        for(int i = 0; i < vec1.length; i++){
+            det -= (vec1.vectorelements[i]*vec2.vectorelements[i]);
+        }
+        return det;
     }
 
-    double veclength3D(Vector3D vector) {
-        return vector.veclength();
+    static Vector crossProduct3D(Vector3D vec1, Vector3D vec2){
+        Vector3D vec = new Vector3D();
+        vec.vectorelements[0]= (vec1.vectorelements[1]*vec2.vectorelements[2])-(vec1.vectorelements[2]*vec2.vectorelements[1]);
+        vec.vectorelements[1]= (vec1.vectorelements[2]*vec2.vectorelements[0])-(vec1.vectorelements[0]*vec2.vectorelements[2]);
+        vec.vectorelements[2]= (vec1.vectorelements[0]*vec2.vectorelements[1])-(vec1.vectorelements[1]*vec2.vectorelements[0]);
+        return vec;
     }
 
-    Vector normalize2D(Vector2D vector){
-        vector.normalize();
-        return vector;
+    static double dotProduct2D(Vector2D vec1, Vector2D vec2){
+        double dotProduct = 0;
+        for(int i = 0; i < vec1.length; i++){
+            dotProduct += (vec1.vectorelements[i]*vec2.vectorelements[i]);
+        }
+        return dotProduct;
     }
-    Vector normalize3D(Vector2D vector){
-        vector.normalize();
-        return vector;
+
+    static double dotProduct3D(Vector3D vec1, Vector3D vec2){
+        double dotProduct = 0;
+        for(int i = 0; i < vec1.length; i++){
+            dotProduct += (vec1.vectorelements[i]*vec2.vectorelements[i]);
+        }
+        return dotProduct;
+    }
+
+    
+
+
+    public static void main(String[] args)
+    {
+        Vector2D vec1 = new Vector2D(3,1);
+        Vector2D vec2 = new Vector2D(2,3);
+        double dist = 0;
+        dist = LineareAlgebra.dotProduct2D(vec1,vec2);
+        System.out.print(dist);
+
     }
 }
--- src/Vector2D.java
+++ src/Vector2D.java
@@ -14,6 +14,7 @@ public class Vector2D extends Vector
     }
     //Copy Constuctur
     public Vector2D(Vector2D vector2D){
+
         super(vector2D);
     }
 
