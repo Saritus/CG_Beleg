c12f9dc1891e8e50406ae1afc87693a50ad2232a
Sebastian Mischke <SebastianMischke@gmx.de>
2016-05-27 15:20:51 +0200

    Extrahieren der readFromFile-Methode

--- src/Schwarmverhalten/Shader.java
+++ src/Schwarmverhalten/Shader.java
@@ -1,4 +1,5 @@
 package Schwarmverhalten;
+
 import org.lwjgl.opengl.Display;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -9,97 +10,83 @@ import static org.lwjgl.opengl.GL20.*;
 /**
  * Created by Matze on 27.05.16.
  */
-public class Shader
-{
-    int shaderProgramm = glCreateProgram();
-    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+public class Shader {
+	int shaderProgramm = glCreateProgram();
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
-    public int getShaderProgrammVar() {
-        return shaderProgramm;
-    }
+	public int getShaderProgrammVar() {
+		return shaderProgramm;
+	}
 
-    public int getVertexShader() {
-        return vertexShader;
-    }
+	public int getVertexShader() {
+		return vertexShader;
+	}
 
-    public int getFragmentShader() {
-        return fragmentShader;
-    }
+	public int getFragmentShader() {
+		return fragmentShader;
+	}
 
+	public void getShaderProgramm() {
 
-    public void getShaderProgramm(){
+		shaderProgramm = glCreateProgram();
+		vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
-        shaderProgramm = glCreateProgram();
-        vertexShader = glCreateShader(GL_VERTEX_SHADER);
-        fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		String vertexShaderSource = readFromFile("vert.shader");
+		String fragmentShaderSource = readFromFile("frag.shader");
 
-        StringBuilder vertexShaderSource = new StringBuilder();
-        StringBuilder fragmentShaderSource = new StringBuilder();
+		// Lets go
+		glShaderSource(vertexShader, vertexShaderSource);
+		glCompileShader(vertexShader);
+		System.out.println(glGetShaderInfoLog(vertexShader, 1024));
 
-        // Vertexshader
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("src/Schwarmverhalten/vert.shader"));
-            String line;
-            while((line = reader.readLine())!= null){
-                vertexShaderSource.append(line).append("\n");
-            }
-            reader.close();
-        }
-        catch (IOException e){
-            System.err.println("Vertexshader wasnt loaded properly");
-            Display.destroy();
+		glShaderSource(fragmentShader, fragmentShaderSource);
+		glCompileShader(fragmentShader);
+		System.out.println(glGetShaderInfoLog(fragmentShader, 1024));
 
-        }
-        // Fragmentshader
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("src/Schwarmverhalten/frag.shader"));
-            String line;
-            while((line = reader.readLine())!= null){
-                fragmentShaderSource.append(line).append("\n");
-            }
-            reader.close();
-        }
-        catch (IOException e){
-            System.err.println("Fragmentshader wasnt loaded properly");
-            Display.destroy();
+		glAttachShader(shaderProgramm, vertexShader);
+		glAttachShader(shaderProgramm, fragmentShader);
+		glLinkProgram(shaderProgramm);
 
-        }
+		glValidateProgram(shaderProgramm);
 
+	}
 
-        // Lets go
-        glShaderSource(vertexShader,vertexShaderSource);
-        glCompileShader(vertexShader);
-        System.out.println(glGetShaderInfoLog(vertexShader,1024));
+	public void useShaderBeforeGL_Begin() {
+		glUseProgram(shaderProgramm);
+	}
 
-        glShaderSource(fragmentShader,fragmentShaderSource);
-        glCompileShader(fragmentShader);
-        System.out.println(glGetShaderInfoLog(fragmentShader,1024));
+	public void useShaderAfterGL_End() {
+		glUseProgram(shaderProgramm);
+	}
 
+	public void deleteShader() {
 
-        glAttachShader(shaderProgramm,vertexShader);
-        glAttachShader(shaderProgramm,fragmentShader);
-        glLinkProgram(shaderProgramm);
+		glDetachShader(shaderProgramm, vertexShader);
+		glDetachShader(shaderProgramm, fragmentShader);
+		glDeleteProgram(shaderProgramm);
+		glDeleteShader(vertexShader);
+		glDeleteShader(fragmentShader);
+	}
 
-        glValidateProgram(shaderProgramm);
+	public static String readFromFile(String name) {
+		StringBuilder source = new StringBuilder();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(name));
 
+			String line;
+			while ((line = reader.readLine()) != null) {
+				source.append(line).append("\n");
+			}
 
+			reader.close();
+		} catch (Exception e) {
+			System.err.println("Error loading source code: " + name);
+			e.printStackTrace();
+		}
 
-    }
-
-    public void useShaderBeforeGL_Begin(){
-        glUseProgram(shaderProgramm);
-    }
-    public void useShaderAfterGL_End(){
-        glUseProgram(shaderProgramm);
-    }
-    public void deleteShader(){
-
-        glDetachShader(shaderProgramm,vertexShader);
-        glDetachShader(shaderProgramm,fragmentShader);
-        glDeleteProgram(shaderProgramm);
-        glDeleteShader(vertexShader);
-        glDeleteShader(fragmentShader);
-    }
+		return source.toString();
+	}
 
 }
