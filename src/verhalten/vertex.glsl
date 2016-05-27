b547db86183dd77a648502a4bf753d6c511cd236
Sebastian Mischke <SebastianMischke@gmx.de>
2016-05-27 22:27:20 +0200

    float-Typ-Konvertierung

--- src/verhalten/vertex.glsl
+++ src/verhalten/vertex.glsl
@@ -4,4 +4,6 @@ varying vec3 color;
 void main() {
 	gl_Position = ftransform();
 	//color.xy = gl_Position.xy;
+	float colordif = 1. - 2.*(speed.x + speed.y);
+	color.xyz = vec3(1, colordif, colordif);
 }
