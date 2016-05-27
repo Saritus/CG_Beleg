721fe0268922153dc2518038988ddc0babd3ae3e
Sebastian Mischke <SebastianMischke@gmx.de>
2016-05-27 20:42:29 +0200

    Beheben von Konvertierungsproblemen

--- src/verhalten/vertex.glsl
+++ src/verhalten/vertex.glsl
@@ -4,6 +4,7 @@ varying vec3 color;
 void main() {
 	gl_Position = ftransform();
 	//color.xy = gl_Position.xy;
-	color.x = 0;
-	color.yz = 1.-2*(speed.x + speed.y);
+	color.x = 0.;
+	color.y = 1.-2.*(speed.x + speed.y);
+	color.z = 1.-2.*(speed.x + speed.y);
 }
