3801a8acbcd56e01be94c052fe185cf316edd918
Sebastian Mischke <SebastianMischke@gmx.de>
2016-05-27 17:02:37 +0200

    Hinzufügen der JUnit-Tests

---
+++ src/test/TestLineareAlgebra.java
@@ -0,0 +1,314 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import math.*;
+
+public class TestLineareAlgebra {
+
+	@Test
+	public void testIsEqual() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = new Vektor2D(1, 2);
+		Vektor2D v3 = new Vektor2D(3, 4);
+		if (!LineareAlgebra.isEqual(v1, v2)) {
+			Assert.fail();
+		}
+		if (LineareAlgebra.isEqual(v1, v3)) {
+			Assert.fail();
+		}
+
+		// Test for Vektor3D
+		Vektor3D v4 = new Vektor3D(1, 2, 4);
+		Vektor3D v5 = new Vektor3D(1, 2, 4);
+		Vektor3D v6 = new Vektor3D(3, 4, 6);
+		if (!LineareAlgebra.isEqual(v4, v5)) {
+			Assert.fail();
+		}
+		if (LineareAlgebra.isEqual(v4, v6)) {
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testIsNotEqual() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = new Vektor2D(1, 2);
+		Vektor2D v3 = new Vektor2D(3, 4);
+		if (LineareAlgebra.isNotEqual(v1, v2)) {
+			Assert.fail();
+		}
+		if (!LineareAlgebra.isNotEqual(v1, v3)) {
+			Assert.fail();
+		}
+
+		// Test for Vektor3D
+		Vektor3D v4 = new Vektor3D(1, 2, 4);
+		Vektor3D v5 = new Vektor3D(1, 2, 4);
+		Vektor3D v6 = new Vektor3D(3, 4, 6);
+		if (LineareAlgebra.isNotEqual(v4, v5)) {
+			Assert.fail();
+		}
+		if (!LineareAlgebra.isNotEqual(v4, v6)) {
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testAdd() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = new Vektor2D(3, 4);
+		Vektor2D v3 = (Vektor2D) LineareAlgebra.add(v1, v2);
+		Assert.assertEquals(4, v3.getElem(0), 0.01);
+		Assert.assertEquals(6, v3.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v4 = new Vektor3D(1, 2, 4);
+		Vektor3D v5 = new Vektor3D(3, 4, 6);
+		Vektor3D v6 = (Vektor3D) LineareAlgebra.add(v4, v5);
+		Assert.assertEquals(4, v6.getElem(0), 0.01);
+		Assert.assertEquals(6, v6.getElem(1), 0.01);
+		Assert.assertEquals(10, v6.getElem(2), 0.01);
+	}
+
+	@Test
+	public void testSub() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = new Vektor2D(3, 4);
+		Vektor2D v3 = (Vektor2D) LineareAlgebra.sub(v1, v2);
+		Assert.assertEquals(-2, v3.getElem(0), 0.01);
+		Assert.assertEquals(-2, v3.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v4 = new Vektor3D(1, 2, 7);
+		Vektor3D v5 = new Vektor3D(3, 4, 6);
+		Vektor3D v6 = (Vektor3D) LineareAlgebra.sub(v4, v5);
+		Assert.assertEquals(-2, v6.getElem(0), 0.01);
+		Assert.assertEquals(-2, v6.getElem(1), 0.01);
+		Assert.assertEquals(1, v6.getElem(2), 0.01);
+	}
+
+	@Test
+	public void testMult() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = (Vektor2D) LineareAlgebra.mult(v1, 2);
+		Assert.assertEquals(2, v2.getElem(0), 0.01);
+		Assert.assertEquals(4, v2.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(1, 2, 7);
+		Vektor3D v4 = (Vektor3D) LineareAlgebra.mult(v3, 3);
+		Assert.assertEquals(3, v4.getElem(0), 0.01);
+		Assert.assertEquals(6, v4.getElem(1), 0.01);
+		Assert.assertEquals(21, v4.getElem(2), 0.01);
+	}
+
+	@Test
+	public void testDiv() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = (Vektor2D) LineareAlgebra.div(v1, 2);
+		Assert.assertEquals(1. / 2., v2.getElem(0), 0.01);
+		Assert.assertEquals(1, v2.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(1, 2, 7);
+		Vektor3D v4 = (Vektor3D) LineareAlgebra.div(v3, 3);
+		Assert.assertEquals(1. / 3., v4.getElem(0), 0.01);
+		Assert.assertEquals(2. / 3., v4.getElem(1), 0.01);
+		Assert.assertEquals(7. / 3., v4.getElem(2), 0.01);
+	}
+
+	@Test
+	public void testNegate() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = (Vektor2D) LineareAlgebra.negate(v1);
+		Assert.assertEquals(-1, v2.getElem(0), 0.01);
+		Assert.assertEquals(-2, v2.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(1, -2, 7);
+		Vektor3D v4 = (Vektor3D) LineareAlgebra.negate(v3);
+		Assert.assertEquals(-1, v4.getElem(0), 0.01);
+		Assert.assertEquals(2, v4.getElem(1), 0.01);
+		Assert.assertEquals(-7, v4.getElem(2), 0.01);
+	}
+
+	@Test
+	public void testLength() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Assert.assertEquals(Math.sqrt(5), v1.length(), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v2 = new Vektor3D(1, -2, 7);
+		Assert.assertEquals(Math.sqrt(54), v2.length(), 0.01);
+	}
+
+	@Test
+	public void testNormalize() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(0, 3);
+		Vektor2D v2 = (Vektor2D) LineareAlgebra.normalize(v1);
+		Assert.assertEquals(0, v2.getElem(0), 0.01);
+		Assert.assertEquals(1, v2.getElem(1), 0.01);
+		Assert.assertEquals(1, v2.length(), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(1, 2, 2);
+		Vektor3D v4 = (Vektor3D) LineareAlgebra.normalize(v3);
+		Assert.assertEquals(1. / 3., v4.getElem(0), 0.01);
+		Assert.assertEquals(2. / 3., v4.getElem(1), 0.01);
+		Assert.assertEquals(2. / 3., v4.getElem(2), 0.01);
+		Assert.assertEquals(1, v4.length(), 0.01);
+	}
+
+	@Test
+	public void testAbs() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(-3, -5);
+		Vektor2D v2 = (Vektor2D) LineareAlgebra.abs(v1);
+		Assert.assertEquals(3, v2.getElem(0), 0.01);
+		Assert.assertEquals(5, v2.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(-3, -5, 7);
+		Vektor3D v4 = (Vektor3D) LineareAlgebra.abs(v3);
+		Assert.assertEquals(3, v4.getElem(0), 0.01);
+		Assert.assertEquals(5, v4.getElem(1), 0.01);
+		Assert.assertEquals(7, v4.getElem(2), 0.01);
+	}
+
+	@Test
+	public void testDeterminante() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = new Vektor2D(3, 4);
+		Assert.assertEquals(-2, LineareAlgebra.determinante(v1, v2), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(0, 3, 1);
+		Vektor3D v4 = new Vektor3D(1, 2, 1);
+		Vektor3D v5 = new Vektor3D(2, 1, 0);
+		Assert.assertEquals(3, LineareAlgebra.determinante(v3, v4, v5), 0.01);
+	}
+
+	@Test
+	public void testEuklDistance() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = new Vektor2D(3, 4);
+		Assert.assertEquals(Math.sqrt(8), LineareAlgebra.euklDistance(v1, v2), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(4, 1, -2);
+		Vektor3D v4 = new Vektor3D(2, 3, -1);
+		Assert.assertEquals(3, LineareAlgebra.euklDistance(v3, v4), 0.01);
+	}
+
+	@Test
+	public void testManhattenDistance() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = new Vektor2D(3, 4);
+		Assert.assertEquals(4, LineareAlgebra.manhattanDistance(v1, v2), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(4, 1, -2);
+		Vektor3D v4 = new Vektor3D(2, 3, -1);
+		Assert.assertEquals(5, LineareAlgebra.manhattanDistance(v3, v4), 0.01);
+	}
+
+	@Test
+	public void testDotProduct() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = new Vektor2D(3, 4);
+		Assert.assertEquals(11, LineareAlgebra.dotProduct(v1, v2), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(1, 2, 3);
+		Vektor3D v4 = new Vektor3D(-7, 8, 9);
+		Assert.assertEquals(36, LineareAlgebra.dotProduct(v3, v4), 0.01);
+	}
+
+	@Test
+	public void testCrossProduct() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 4);
+		Vektor2D v2 = new Vektor2D(3, 4);
+		Assert.assertEquals(8, LineareAlgebra.crossProduct(v1, v2).length(), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v4 = new Vektor3D(1, -5, 10);
+		Vektor3D v5 = new Vektor3D(2, 0, 20);
+		Vektor3D v6 = (Vektor3D) LineareAlgebra.crossProduct(v4, v5);
+		Assert.assertArrayEquals(new double[] { -100, 0, 10 }, v6.getArray(), 0.01);
+	}
+
+	@Test
+	public void testCosEquation() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(0, 4);
+		Vektor2D v2 = new Vektor2D(3, 0);
+		Assert.assertEquals(0, LineareAlgebra.cosEquation(v1, v2), 0.0001);
+
+		// Test for Vektor3D
+		Vektor3D v4 = new Vektor3D(1, -5, 10);
+		Vektor3D v5 = new Vektor3D(2, 0, 20);
+		Assert.assertEquals(0.8953140798, LineareAlgebra.cosEquation(v4, v5), 0.0001);
+	}
+
+	@Test
+	public void testSinEquation() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(0, 4);
+		Vektor2D v2 = new Vektor2D(3, 0);
+		Assert.assertEquals(1, LineareAlgebra.sinEquation(v1, v2), 0.0001);
+
+		// Test for Vektor3D
+		Vektor3D v4 = new Vektor3D(-10, 0, 10);
+		Vektor3D v5 = new Vektor3D(2, 30, 0);
+		Assert.assertEquals(0.998893192, LineareAlgebra.sinEquation(v4, v5), 0.0001);
+	}
+
+	@Test
+	public void testAngleDegree() throws Exception {
+		Assert.assertEquals(30, LineareAlgebra.angleDegree(0.5, "sin"), 0.01);
+		Assert.assertEquals(45, LineareAlgebra.angleDegree(1. / Math.sqrt(2), "sin"), 0.01);
+		Assert.assertEquals(60, LineareAlgebra.angleDegree(0.5, "cos"), 0.01);
+		Assert.assertEquals(45, LineareAlgebra.angleDegree(1. / Math.sqrt(2), "cos"), 0.01);
+		Assert.assertEquals(45, LineareAlgebra.angleDegree(1, "tan"), 0.01);
+	}
+
+	@Test
+	public void testAngleRad() throws Exception {
+		Assert.assertEquals(Math.PI / 6, LineareAlgebra.angleRad(0.5, "sin"), 0.01);
+		Assert.assertEquals(Math.PI / 4, LineareAlgebra.angleRad(1. / Math.sqrt(2), "sin"), 0.01);
+		Assert.assertEquals(Math.PI / 3, LineareAlgebra.angleRad(0.5, "cos"), 0.01);
+		Assert.assertEquals(Math.PI / 4, LineareAlgebra.angleRad(1. / Math.sqrt(2), "cos"), 0.01);
+		Assert.assertEquals(Math.PI / 4, LineareAlgebra.angleRad(1, "tan"), 0.01);
+	}
+
+	@Test
+	public void testRadToDegree() throws Exception {
+		Assert.assertEquals(0, LineareAlgebra.radToDegree(0), 0.01);
+		Assert.assertEquals(90, LineareAlgebra.radToDegree(Math.PI / 2), 0.01);
+		Assert.assertEquals(180, LineareAlgebra.radToDegree(Math.PI), 0.01);
+		Assert.assertEquals(270, LineareAlgebra.radToDegree(3 * Math.PI / 2), 0.01);
+	}
+
+	@Test
+	public void testDegreeToRad() throws Exception {
+		Assert.assertEquals(0, LineareAlgebra.degreeToRad(0), 0.01);
+		Assert.assertEquals(Math.PI / 2, LineareAlgebra.degreeToRad(90), 0.01);
+		Assert.assertEquals(Math.PI, LineareAlgebra.degreeToRad(180), 0.01);
+		Assert.assertEquals(3 * Math.PI / 2, LineareAlgebra.degreeToRad(270), 0.01);
+	}
+}
---
+++ src/test/TestVektor.java
@@ -0,0 +1,471 @@
+package test;
+
+import org.junit.*;
+import math.*;
+
+public class TestVektor {
+
+	@Test
+	public void testGetDimension() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v2d = new Vektor2D();
+		Assert.assertEquals(v2d.getDimension(), 2);
+
+		// Test for Vektor3D
+		Vektor3D v3d = new Vektor3D();
+		Assert.assertEquals(v3d.getDimension(), 3);
+	}
+
+	@Test
+	public void testGetArray() throws Exception {
+		// Test for Vektor2D
+		double[] expecteds = { 2, 3 };
+		Vektor2D v2d = new Vektor2D(expecteds);
+		Assert.assertArrayEquals(expecteds, v2d.getArray(), 0.01);
+
+		// Test for Vektor3D
+		expecteds = new double[] { 2, 3, 5 };
+		Vektor3D v3d = new Vektor3D(expecteds);
+		Assert.assertArrayEquals(expecteds, v3d.getArray(), 0.01);
+	}
+
+	@Test
+	public void testGetElemPositive() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v2d = new Vektor2D(2, 3);
+		Assert.assertEquals(2, v2d.getElem(0), 0.01);
+		Assert.assertEquals(3, v2d.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3d = new Vektor3D(2, 3, 5);
+		Assert.assertEquals(2, v3d.getElem(0), 0.01);
+		Assert.assertEquals(3, v3d.getElem(1), 0.01);
+		Assert.assertEquals(5, v3d.getElem(2), 0.01);
+	}
+
+	@Test
+	public void testGetElemNegative() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v2d = new Vektor2D(2, 3);
+		try {
+			// Negative OutOfBounds
+			v2d.getElem(-1);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Invalid dimension", e.getMessage());
+		}
+		try {
+			// Positive OutOfBounds
+			v2d.getElem(4);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Invalid dimension", e.getMessage());
+		}
+
+		// Test for Vektor3D
+		Vektor3D v3d = new Vektor3D(2, 3, 5);
+		try {
+			// Negative OutOfBounds
+			v3d.getElem(-1);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Invalid dimension", e.getMessage());
+		}
+		try {
+			// Positive OutOfBounds
+			v3d.getElem(5);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Invalid dimension", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testSetPositionPositive() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v2d = new Vektor2D();
+		double[] expecteds = { 2., 3. };
+		v2d.setPosition(expecteds);
+		Assert.assertArrayEquals(expecteds, v2d.getArray(), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3d = new Vektor3D();
+		expecteds = new double[] { 2., 3., 5. };
+		v3d.setPosition(expecteds);
+		Assert.assertArrayEquals(expecteds, v3d.getArray(), 0.01);
+	}
+
+	@Test
+	public void testSetPositionNegative() throws Exception {
+		// Test for Vektor2D
+		try {
+			Vektor2D v2d = new Vektor2D();
+			v2d.setPosition(2, 3, 5);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Array does not have the right length", e.getMessage());
+		}
+
+		// Test for Vektor3D
+		try {
+			Vektor3D v3d = new Vektor3D();
+			v3d.setPosition(new double[] { 2, 3, 5, 9 });
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Array does not have the right length", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testIsNullVector() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(0, 0);
+		Vektor2D v2 = new Vektor2D(0, 3);
+		if (!v1.isNullVector()) {
+			Assert.fail();
+		}
+		if (v2.isNullVector()) {
+			Assert.fail();
+		}
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(0, 0, 0);
+		Vektor3D v4 = new Vektor3D(1, 1, 2);
+		if (!v3.isNullVector()) {
+			Assert.fail();
+		}
+		if (v4.isNullVector()) {
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testAddPositive() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(2, 3);
+		Vektor2D v2 = new Vektor2D(5, 7);
+		v1.add(v2);
+		Assert.assertEquals(7, v1.getElem(0), 0.01);
+		Assert.assertEquals(10, v1.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(2, 3, 11);
+		Vektor3D v4 = new Vektor3D(5, 7, 13);
+		v3.add(v4);
+		Assert.assertEquals(7, v3.getElem(0), 0.01);
+		Assert.assertEquals(10, v3.getElem(1), 0.01);
+		Assert.assertEquals(24, v3.getElem(2), 0.01);
+	}
+
+	@Test
+	public void testAddNegative() throws Exception {
+		// Test for Vektor2D
+		try {
+			// Positive Overflow
+			Vektor2D v1 = new Vektor2D(Double.MAX_VALUE, Double.MAX_VALUE);
+			Vektor2D v2 = new Vektor2D(Double.MAX_VALUE, Double.MAX_VALUE);
+			v1.add(v2);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Positive double overflow", e.getMessage());
+		}
+		try {
+			// Negative Overflow
+			Vektor2D v1 = new Vektor2D(-Double.MAX_VALUE, -Double.MAX_VALUE);
+			Vektor2D v2 = new Vektor2D(-Double.MAX_VALUE, -Double.MAX_VALUE);
+			v1.add(v2);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Negative double overflow", e.getMessage());
+		}
+
+		// Test for Vektor3D
+		try {
+			// Positive Overflow
+			Vektor3D v1 = new Vektor3D(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+			Vektor3D v2 = new Vektor3D(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+			v1.add(v2);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Positive double overflow", e.getMessage());
+		}
+		try {
+			// Negative Overflow
+			Vektor3D v1 = new Vektor3D(-Double.MAX_VALUE, -Double.MAX_VALUE, -Double.MAX_VALUE);
+			Vektor3D v2 = new Vektor3D(-Double.MAX_VALUE, -Double.MAX_VALUE, -Double.MAX_VALUE);
+			v1.add(v2);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Negative double overflow", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testSub() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(2, 3);
+		Vektor2D v2 = new Vektor2D(5, 7);
+		v1.sub(v2);
+		Assert.assertEquals(-3, v1.getElem(0), 0.01);
+		Assert.assertEquals(-4, v1.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(2, 3, 11);
+		Vektor3D v4 = new Vektor3D(5, 7, 13);
+		v3.sub(v4);
+		Assert.assertEquals(-3, v3.getElem(0), 0.01);
+		Assert.assertEquals(-4, v3.getElem(1), 0.01);
+		Assert.assertEquals(-2, v3.getElem(2), 0.01);
+	}
+
+	@Test
+	public void testMultPositive() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(2, 3);
+		v1.mult(3);
+		Assert.assertEquals(6, v1.getElem(0), 0.01);
+		Assert.assertEquals(9, v1.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(2, 3, 11);
+		v3.mult(4);
+		Assert.assertEquals(8, v3.getElem(0), 0.01);
+		Assert.assertEquals(12, v3.getElem(1), 0.01);
+		Assert.assertEquals(44, v3.getElem(2), 0.01);
+	}
+
+	@Test
+	public void testMultNegative() {
+		// Test for Vektor2D
+		try {
+			// Positive Overflow
+			Vektor2D v1 = new Vektor2D(Double.MAX_VALUE, Double.MAX_VALUE);
+			v1.mult(2);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Positive double overflow", e.getMessage());
+		}
+		try {
+			// Negative Overflow
+			Vektor2D v1 = new Vektor2D(-Double.MAX_VALUE, -Double.MAX_VALUE);
+			v1.mult(3);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Negative double overflow", e.getMessage());
+		}
+
+		// Test for Vektor3D
+		try {
+			// Positive Overflow
+			Vektor3D v1 = new Vektor3D(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+			v1.mult(3);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Positive double overflow", e.getMessage());
+		}
+		try {
+			// Negative Overflow
+			Vektor3D v1 = new Vektor3D(-Double.MAX_VALUE, -Double.MAX_VALUE, -Double.MAX_VALUE);
+			v1.mult(4);
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Negative double overflow", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testDivPositive() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(6, 8);
+		v1.div(2);
+		Assert.assertEquals(3, v1.getElem(0), 0.01);
+		Assert.assertEquals(4, v1.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v2 = new Vektor3D(6, 9, 12);
+		v2.div(3);
+		Assert.assertEquals(2, v2.getElem(0), 0.01);
+		Assert.assertEquals(3, v2.getElem(1), 0.01);
+		Assert.assertEquals(4, v2.getElem(2), 0.01);
+	}
+
+	@Test(expected = Exception.class)
+	public void testDivNegative() throws Exception {
+		Vektor2D v1 = new Vektor2D(6, 8);
+		v1.div(0);
+	}
+
+	@Test
+	public void testIsEqual() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = new Vektor2D(1, 2);
+		Vektor2D v3 = new Vektor2D(2, 3);
+		if (!v1.isEqual(v2)) {
+			Assert.fail();
+		}
+		if (v1.isEqual(v3)) {
+			Assert.fail();
+		}
+
+		// Test for Vektor3D
+		Vektor3D v4 = new Vektor3D(1, 2, 4);
+		Vektor3D v5 = new Vektor3D(1, 2, 4);
+		Vektor3D v6 = new Vektor3D(2, 3, 6);
+		if (!v4.isEqual(v5)) {
+			Assert.fail();
+		}
+		if (v4.isEqual(v6)) {
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testIsNotEqual() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = new Vektor2D(1, 2);
+		Vektor2D v3 = new Vektor2D(2, 3);
+		if (v1.isNotEqual(v2)) {
+			Assert.fail();
+		}
+		if (!v1.isNotEqual(v3)) {
+			Assert.fail();
+		}
+
+		// Test for Vektor3D
+		Vektor3D v4 = new Vektor3D(1, 2, 4);
+		Vektor3D v5 = new Vektor3D(1, 2, 4);
+		Vektor3D v6 = new Vektor3D(2, 3, 6);
+		if (v4.isNotEqual(v5)) {
+			Assert.fail();
+		}
+		if (!v4.isNotEqual(v6)) {
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testLength() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(2, 2);
+		Assert.assertEquals(Math.sqrt(8), v1.length(), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v2 = new Vektor3D(2, 2, 3);
+		Assert.assertEquals(Math.sqrt(17), v2.length(), 0.01);
+	}
+
+	@Test
+	public void testLengthsquare() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(2, 2);
+		Assert.assertEquals(8, v1.lengthsquare(), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v2 = new Vektor3D(2, 2, 3);
+		Assert.assertEquals(17, v2.lengthsquare(), 0.01);
+	}
+
+	@Test
+	public void testNormalizePositive() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(0, 3);
+		v1.normalize();
+		Assert.assertEquals(0, v1.getElem(0), 0.01);
+		Assert.assertEquals(1, v1.getElem(1), 0.01);
+		Assert.assertEquals(1, v1.length(), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v2 = new Vektor3D(1, 2, 2);
+		v2.normalize();
+		Assert.assertEquals(1. / 3., v2.getElem(0), 0.01);
+		Assert.assertEquals(2. / 3., v2.getElem(1), 0.01);
+		Assert.assertEquals(2. / 3., v2.getElem(2), 0.01);
+		Assert.assertEquals(1, v2.length(), 0.01);
+	}
+
+	@Test
+	public void testNormalizeNegative() throws Exception {
+		// Test for Vektor2D
+		try {
+			Vektor2D v1 = new Vektor2D(0, 0);
+			v1.normalize();
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Nullvector cannot be normalized", e.getMessage());
+		}
+
+		// Test for Vektor3D
+		try {
+			Vektor3D v2 = new Vektor3D(0, 0, 0);
+			v2.normalize();
+			Assert.fail();
+		} catch (Exception e) {
+			Assert.assertEquals("Nullvector cannot be normalized", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testNegate() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(2, 3);
+		v1.negate();
+		Assert.assertEquals(-2, v1.getElem(0), 0.01);
+		Assert.assertEquals(-3, v1.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v2 = new Vektor3D(2, 3, -4);
+		v2.negate();
+		Assert.assertEquals(-2, v2.getElem(0), 0.01);
+		Assert.assertEquals(-3, v2.getElem(1), 0.01);
+		Assert.assertEquals(4, v2.getElem(2), 0.01);
+	}
+
+	@Test
+	public void testClone() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(1, 2);
+		Vektor2D v2 = v1.clone();
+		Assert.assertEquals(1, v2.getElem(0), 0.01);
+		Assert.assertEquals(2, v2.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(1, 2, -4);
+		Vektor3D v4 = v3.clone();
+		Assert.assertEquals(1, v4.getElem(0), 0.01);
+		Assert.assertEquals(2, v4.getElem(1), 0.01);
+		Assert.assertEquals(-4, v4.getElem(2), 0.01);
+	}
+
+	@Test
+	public void testAbs() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(-1, -2);
+		v1.abs();
+		Assert.assertEquals(1, v1.getElem(0), 0.01);
+		Assert.assertEquals(2, v1.getElem(1), 0.01);
+
+		// Test for Vektor3D
+		Vektor3D v2 = new Vektor3D(-1, 2, -4);
+		v2.abs();
+		Assert.assertEquals(1, v2.getElem(0), 0.01);
+		Assert.assertEquals(2, v2.getElem(1), 0.01);
+		Assert.assertEquals(4, v2.getElem(2), 0.01);
+	}
+
+	@Test
+	public void testToString() throws Exception {
+		// Test for Vektor2D
+		Vektor2D v1 = new Vektor2D(-1, -2);
+		Vektor2D v2 = new Vektor2D(-1.333, Math.PI);
+		Assert.assertEquals("( -1.0, -2.0 )", v1.toString());
+		Assert.assertEquals("( -1.333, 3.141592653589793 )", v2.toString());
+
+		// Test for Vektor3D
+		Vektor3D v3 = new Vektor3D(-1, -2, 7);
+		Vektor3D v4 = new Vektor3D(-1.333, Math.PI, Math.E);
+		Assert.assertEquals("( -1.0, -2.0, 7.0 )", v3.toString());
+		Assert.assertEquals("( -1.333, 3.141592653589793, 2.718281828459045 )", v4.toString());
+	}
+}
