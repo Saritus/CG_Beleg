d3d54aa5a1873bccac5568c13da7af0d9b0f7a96
Sebastian Mischke <SebastianMischke@gmx.de>
2016-07-15 15:45:26 +0200

    Verbessern der Tests

--- src/test/TestBeweglichesObjekt.java
+++ src/test/TestBeweglichesObjekt.java
@@ -1,34 +1,47 @@
 package test;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import math.Vektor2D;
 import objekte.SchwarmObjekt;
+import verhalten.ObjektManager;
 
 public class TestBeweglichesObjekt {
-	@Test
-	public void testCalculateDistances() {
-		SchwarmObjekt so1 = new SchwarmObjekt(new Vektor2D(), new Vektor2D(), 1);
-		SchwarmObjekt so2 = new SchwarmObjekt(new Vektor2D(1, 0), new Vektor2D(), 1);
+
+	SchwarmObjekt so1, so2;
+
+	@Before
+	public void initObjects() {
+		so1 = new SchwarmObjekt(new Vektor2D(), new Vektor2D(), 1);
+		so2 = new SchwarmObjekt(new Vektor2D(1, 1), new Vektor2D(), 1);
 		so1.calculateDistances();
 		so2.calculateDistances();
-		Assert.assertEquals(19., so1.getAbstand()[0], 0.01);
-		Assert.assertEquals(20., so1.getAbstand()[1], 0.01);
-		Assert.assertEquals(18., so2.getAbstand()[0], 0.01);
-		Assert.assertEquals(19., so2.getAbstand()[1], 0.01);
+	}
+
+	@After
+	public void deleteObjects() {
+		ObjektManager.getInstance().removeAll();
+	}
+
+	@Test
+	public void testCalculateDistances() {
+		Assert.assertEquals(0., so1.getAbstand()[0], 0.01);
+		Assert.assertEquals(2., so1.getAbstand()[1], 0.01);
+		Assert.assertEquals(2., so2.getAbstand()[0], 0.01);
+		Assert.assertEquals(0., so2.getAbstand()[1], 0.01);
 	}
 
 	@Test
 	public void testEulerMethod() {
-		SchwarmObjekt so1 = new SchwarmObjekt(new Vektor2D(5, 5), new Vektor2D(), 1);
-		SchwarmObjekt so2 = new SchwarmObjekt(new Vektor2D(10, 10), new Vektor2D(), 1);
 		so1.eulerMethod(new Vektor2D(5, 4));
 		so1.calculateDistances();
 		so2.calculateDistances();
 		Assert.assertEquals(0., so1.getAbstand()[0], 0.01);
-		Assert.assertEquals(1., so1.getAbstand()[1], 0.01);
-		Assert.assertEquals(1., so2.getAbstand()[0], 0.01);
+		Assert.assertEquals(7., so1.getAbstand()[1], 0.01);
+		Assert.assertEquals(7., so2.getAbstand()[0], 0.01);
 		Assert.assertEquals(0., so2.getAbstand()[1], 0.01);
 	}
 }
